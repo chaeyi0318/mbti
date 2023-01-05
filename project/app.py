3492e34b491f4ab5fece2f4f6aa3ce589e048708
Son Chaeyi <chaeyi0318@gmail.com>
2023-01-05 22:35:21 +0900

    html 수정 & 페이지 나누기

--- project/app.py
+++ project/app.py
@@ -5,6 +5,18 @@ from pymongo import MongoClient
 client = MongoClient('mongodb+srv://chaeyi0318:test@cluster0.xa9ngty.mongodb.net/Cluster0?retryWrites=true&w=majority')
 db = client.dbsparta
 
+# title_list = list(db.board.find({}, {'_id': False}))
+#
+# count = len(title_list) + 1
+#
+# doc = {
+#     'index': count,
+#     'title': '제목12입니다.',
+# }
+#
+# db.board.insert_one(doc)
+
+
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -12,9 +24,9 @@ def home():
 @app.route("/board", methods=["GET"])
 def homework_get():
     title_list = list(db.board.find({}, {'_id':False}))
-    print(title_list)
     topic_list = list(db.topic.find({}, {'_id':False}))
-    print(topic_list)
+
+    db.board.find().limit(15)
 
     return jsonify({'all_title': title_list, 'all_topic': topic_list})
 
